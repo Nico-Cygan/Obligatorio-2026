c621b7fd9d1b859bdf882e0f13fe87793900971e
Nico-Cygan <nicocygan18@gmail.com>
2026-06-09 16:51:06 -0300

    Implementación de prioridad y comparación de procesos

--- src/main/java/uy/edu/um/doors/model/Process.java
+++ src/main/java/uy/edu/um/doors/model/Process.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import uy.edu.um.tad.list.MyList;
+import uy.edu.um.tad.list.Node;
 
 @Getter
 @Setter
@@ -14,7 +15,7 @@ import uy.edu.um.tad.list.MyList;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Process {
+public class Process implements Comparable<Process> {
     @EqualsAndHashCode.Include
     private int pid;
     private String name;
@@ -22,4 +23,48 @@ public class Process {
     private int priority;
     private ProcessState state;
     private MyList<Event> events;
+
+    public int calculatePriority() {
+        if (user == null || events == null || events.isEmpty()) {
+            this.priority = 0;
+            return this.priority;
+        }
+
+        int cpuEvents = 0;
+        int ramEvents = 0;
+        int diskEvents = 0;
+        int totalEvents = 0;
+
+        Node<Event> current = events.getFirst();
+        while (current != null) {
+            Event event = current.getValue();
+            if (event != null && event.getType() != null) {
+                switch (event.getType()) {
+                    case CPU -> cpuEvents++;
+                    case RAM -> ramEvents++;
+                    case DISK -> diskEvents++;
+                }
+            }
+            totalEvents++;
+            current = current.getNext();
+        }
+
+        if (totalEvents == 0) {
+            this.priority = 0;
+            return this.priority;
+        }
+
+        int weightedEvents = 8 * cpuEvents + 2 * ramEvents + 2 * diskEvents;
+        int userWeight = user.getType() != null ? user.getType().getWeight() : 0;
+        this.priority = (weightedEvents / totalEvents) + (userWeight * totalEvents);
+        return this.priority;
+    }
+
+    @Override
+    public int compareTo(Process other) {
+        if (other == null) {
+            return 1;
+        }
+        return Integer.compare(this.priority, other.priority);
+    }
 }
