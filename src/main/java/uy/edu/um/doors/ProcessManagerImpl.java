3e91610513bac04b9cd42b67fbc5c5a8660f1ddf
axelvladi34 <axelbulba@gmail.com>
2026-06-11 12:41:45 -0300

    Primer procedimiento:
    Se implemento el primer procedimiento de impresion. Ademas se declararon 3 estructuras de datos para procesos pendientes, terminados y en ejecucucion. Utilizando Heap y Stack respectivamente para los dos primero

--- src/main/java/uy/edu/um/doors/ProcessManagerImpl.java
+++ src/main/java/uy/edu/um/doors/ProcessManagerImpl.java
@@ -4,14 +4,27 @@ import uy.edu.um.doors.model.Process;
 import uy.edu.um.doors.model.User;
 import uy.edu.um.tad.hash.MyHash;
 import uy.edu.um.tad.hash.MyHashImpl;
+import uy.edu.um.tad.heap.EmptyHeapException;
+import uy.edu.um.tad.heap.MyHeap;
+import uy.edu.um.tad.list.MyLinkedListImpl;
+import uy.edu.um.tad.list.MyList;
+import uy.edu.um.tad.list.Node;
 import uy.edu.um.tad.queue.MyQueue;
 import uy.edu.um.tad.queue.MyQueueImpl;
+import uy.edu.um.tad.stack.EmptyStackException;
+import uy.edu.um.tad.stack.MyStack;
+import uy.edu.um.tad.stack.MyStackImpl;
+
+import javax.swing.*;
 
 public class ProcessManagerImpl implements ProcessManager {
 
     private MyHash<Integer, User> usersByUid;
     private MyHash<Integer, Process> processesByPid;
     private MyQueue<Process> newProcesses;
+    private MyHeap<Process> pendingProcesses;
+    private MyStack<Process> finishedProcesses;
+    private Process currentProcess;
 
     public ProcessManagerImpl() {
         this.usersByUid = new MyHashImpl<>();
@@ -51,7 +64,54 @@ public class ProcessManagerImpl implements ProcessManager {
 
     @Override
     public void printStatus() {
+        System.out.println("PROCESS STATUS");
+        System.out.printf("EXECUTING:");
+        if (currentProcess != null){
+            System.out.println("\t" + printOneProcess(currentProcess));
+        }
+        System.out.println("PENDING");
+        MyList<Process> aux = new MyLinkedListImpl<>();
+        while(!pendingProcesses.isEmpty()){
+            try{
+                Process p = pendingProcesses.remove();
+                aux.add(p);
+                System.out.println("\t" + printOneProcess(p));
+            } catch (EmptyHeapException exc){
+                break;
+            }
+            Node<Process> actual = aux.getFirst();
+            while (actual != null){
+                pendingProcesses.insert(actual.getValue());
+                actual = actual.getNext();
+            }
+            System.out.println("FINISHED:");
+            MyList<Process> auxTerminados = new MyLinkedListImpl<>();
+            while (!finishedProcesses.isEmpty()){
+                try{
+                    Process p = finishedProcesses.pop();
+                    auxTerminados.add(p);
+                    System.out.println("\t" + printOneFinishedProcess(p));
+                } catch (EmptyStackException exc2){
+                    break;
+                }
+            }
+        }
+
+
+
+
+
         System.out.println("IMPLEMENTAR");
+    }
+    public String printOneProcess(Process p){
+        return "PID=" + p.getPid() + " | " + p.getName() + " | USER:"
+                + p.getUser().getAlias() + " UID:" + p.getUser().getUid() + " | P=" + p.getPriority();
+
+    }
+    public String printOneFinishedProcess(Process p){
+        return "PID=" + p.getPid() + " | " + p.getName() + " | STATE: " + p.getState() +
+                " | USER:" + p.getUser().getAlias() + " UID:" + p.getUser().getUid();
+
     }
 
     @Override
